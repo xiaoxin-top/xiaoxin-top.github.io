f059beb123d640df43b3e3a1fe63ed03df1e19b1
xiaoxin-top <littlectx@gmail.com>
2024-06-22 21:25:56 +0800

    Update blog content

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/15/hello-world/","2024/06/15/零成本搭建个人博客/"];function toRandomPost(){
+var posts=["2024/06/15/hello-world/","2024/06/22/tuku/","2024/06/15/零成本搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
