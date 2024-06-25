35c0f3ab450870b1d9f035a2a22493ac29c98e60
xiaoxin-top <littlectx@gmail.com>
2024-06-26 01:12:43 +0800

    Update blog content

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["looking/3766f053.html","looking/5c120c19.html","looking/c8e05f01.html","looking/3610a686.html","looking/f134a8e5.html","looking/4d33b968.html","looking/8f33bcf2.html"];function toRandomPost(){
+var posts=["looking/3766f053.html","looking/c8e05f01.html","looking/5c120c19.html","looking/f134a8e5.html","looking/8f33bcf2.html","looking/4d33b968.html","looking/3610a686.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
