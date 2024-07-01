c3d060e1b820f5a2142908a72fea8b78f95ed892
xiaoxin-top <littlectx@gmail.com>
2024-07-01 22:13:20 +0800

    Update blog content

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["looking/5c120c19.html","looking/3766f053.html","looking/c8e05f01.html","looking/3610a686.html","looking/d67f438b.html","looking/3d4be8d8.html","looking/f134a8e5.html","looking/300f2b.html","looking/1c9e63ed.html","looking/41ec75e8.html","looking/4d33b968.html","looking/8f33bcf2.html"];function toRandomPost(){
+var posts=["looking/3766f053.html","looking/5c120c19.html","looking/c8e05f01.html","looking/3610a686.html","looking/d67f438b.html","looking/3d4be8d8.html","looking/688fa470.html","looking/f134a8e5.html","looking/1c9e63ed.html","looking/41ec75e8.html","looking/300f2b.html","looking/4d33b968.html","looking/8f33bcf2.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
